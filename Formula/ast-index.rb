a17762a6d56437636cc2a481ba60c717efe76ade
defendend <ivsacha@mail.ru>
2026-01-20 19:13:59 +0300

    v3.6.0: Perl support

--- Formula/ast-index.rb
+++ Formula/ast-index.rb
@@ -1,16 +1,16 @@
 class AstIndex < Formula
-  desc "Fast code search CLI for Android/Kotlin/Java and iOS/Swift/ObjC projects"
+  desc "Fast code search CLI for Android/Kotlin/Java, iOS/Swift/ObjC, and Perl projects"
   homepage "https://github.com/defendend/Claude-ast-index-search"
-  url "https://github.com/defendend/Claude-ast-index-search/releases/download/v3.5.0/ast-index-3.5.0-darwin-arm64.tar.gz"
-  sha256 "d6c387707f7908875b8e3e9b1dc34857004e0defd1d42469833cc103ba3ec7c0"
+  url "https://github.com/defendend/Claude-ast-index-search/releases/download/v3.6.0/ast-index-3.6.0-darwin-arm64.tar.gz"
+  sha256 "939600b07d7958f6e42d7b236d103600374f5f5e8c6963446e1b677d826208cc"
   license "MIT"
-  version "3.5.0"
+  version "3.6.0"
 
   def install
     bin.install "ast-index"
   end
 
   test do
-    assert_match "ast-index 3.5.0", shell_output("#{bin}/ast-index version")
+    assert_match "ast-index 3.6.0", shell_output("#{bin}/ast-index version")
   end
 end
