3a65fed1051fa3d819951cb18d0e17fec84c5f02
defendend <ivsacha@mail.ru>
2026-02-02 10:38:29 +0300

    Update to v3.10.0

--- Formula/ast-index.rb
+++ Formula/ast-index.rb
@@ -1,10 +1,23 @@
 class AstIndex < Formula
   desc "Fast code search CLI for Android, iOS, TypeScript, Rust, Ruby, C#, Python, Go, Perl, C++ projects"
   homepage "https://github.com/defendend/Claude-ast-index-search"
-  url "https://github.com/defendend/Claude-ast-index-search/releases/download/v3.10.0/ast-index-3.10.0-darwin-arm64.tar.gz"
-  sha256 "499f3439ce20f00eace09a265478e0bd5859bc273a41ad0addc48ae0a0c870b4"
   license "MIT"
   version "3.10.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/defendend/Claude-ast-index-search/releases/download/v3.10.0/ast-index-v3.10.0-darwin-arm64.tar.gz"
+      sha256 "cbc042a899545a5ebb51c53d3bebb1062a454eec371c50c151ffb5c855792597"
+    else
+      url "https://github.com/defendend/Claude-ast-index-search/releases/download/v3.10.0/ast-index-v3.10.0-darwin-x86_64.tar.gz"
+      sha256 "0e7101dc3692c819754cdab7ac45b608ab52ee0ebfe477e5364191171dd1fc2e"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/defendend/Claude-ast-index-search/releases/download/v3.10.0/ast-index-v3.10.0-linux-x86_64.tar.gz"
+    sha256 "1a3c82652e66fdb512f138ed365a4e00c1e6b0d619927f238d50691d6cb1dbc0"
+  end
 
   def install
     bin.install "ast-index"
